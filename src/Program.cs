ecb60ced9b05a934412ec28adc55d05b67f5c107
BRBergman <blake.bergman@gmail.com>
2022-05-24 14:25:21 -0400

    master

--- src/Program.cs
+++ src/Program.cs
@@ -3,10 +3,11 @@
 
 class Program
 {
-    void Main()
+    static void Main()
     {
-       
-        
+       int i = 1;
+        i = Maths.Invert.InvertI(i);
+        Console.WriteLine(i);
         return;
     }
 
