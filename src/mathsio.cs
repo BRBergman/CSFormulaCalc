b2303057af0a3900f476f416adda572d1db6deec
BRBergman <blake.bergman@gmail.com>
2022-05-25 14:00:57 -0400

    master

--- src/mathsio.cs
+++ src/mathsio.cs
@@ -11,7 +11,7 @@ namespace CSFormulaCalc
             float[] x = (CSFormulaCalc.extras.getvars(str));
             if (inverse)
             {
-              Console.WriteLine("The Leg is: {0}",CSFormulaCalc.Formulas.ReversePythagorean(x[0],x[1]));   
+                Console.WriteLine("The Leg is: {0}",CSFormulaCalc.Formulas.ReversePythagorean(x[0],x[1]));   
             }
             else
             {
